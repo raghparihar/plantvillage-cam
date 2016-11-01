360413e5ee296c6f9a80f0d278a23ae3155bc160
S.P. Mohanty <spmohanty91@gmail.com>
2016-11-01 16:31:08 +0100

    Remove the MaxPooling layer before the GAP layer

--- train.py
+++ train.py
@@ -99,7 +99,6 @@ model.add(ZeroPadding2D((1,1)))
 model.add(Convolution2D(512, 3, 3, activation='relu'))
 model.add(ZeroPadding2D((1,1)))
 model.add(Convolution2D(512, 3, 3, activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2)))
 
 ##
 ## Load Weights
