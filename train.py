d2b2cb9ef2f3f066df602a2d738c2e3888b50ead
S.P. Mohanty <spmohanty91@gmail.com>
2016-11-07 04:59:48 +0100

    Cleanup old redundant functions

--- train.py
+++ train.py
@@ -27,37 +27,6 @@ nb_validation_samples = 2389
 nb_epoch = 50
 
 
-# Define custom GAP layer
-def global_average_pooling(x):
-    return K.mean(x, axis = (2, 3))
-
-def global_average_pooling_shape(input_shape):
-    print input_shape
-    return input_shape[0:2]
-
-#Define helper functions
-def get_output_layer(model, layer_name):
-    # get the symbolic outputs of each "key" layer (we gave them unique names).
-    layer_dict = dict([(layer.name, layer) for layer in model.layers])
-    layer = layer_dict[layer_name]
-    return layer
-
-def load_model_weights(model, weights_path):
-    print 'Loading model.'
-    f = h5py.File(weights_path)
-    for k in range(f.attrs['nb_layers']):
-        if k >= len(model.layers):
-            # we don't look at the last (fully-connected) layers in the savefile
-            break
-        g = f['layer_{}'.format(k)]
-        weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
-        model.layers[k].set_weights(weights)
-        model.layers[k].trainable = False
-    f.close()
-    print 'Model loaded.'
-    return model
-
-
 base_model = VGG16(weights='imagenet', input_tensor=Input((3, 224, 224)),include_top=False)
 # add a global spatial average pooling layer
 x = base_model.output
