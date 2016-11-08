3206c561cbfc29157a7f3e65374c792c7fef58df
S.P. Mohanty <spmohanty91@gmail.com>
2016-11-08 23:24:02 +0100

    Make PLOT_PROJECTIONS conditional

--- predict.py
+++ predict.py
@@ -11,7 +11,7 @@ from keras import backend as K
 from keras.optimizers import SGD
 #
 from keras.callbacks import ModelCheckpoint
-# from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator
 #
 from keras.applications.vgg16 import VGG16
 from keras.applications.vgg16 import preprocess_input
@@ -28,10 +28,16 @@ import json
 
 import uuid
 
+PLOT_PROJECTIONS = True
 
-OUTPUT_DIRECTORY = "/mount/SDC/PROJECTION_RESULTS"
+if PLOT_PROJECTIONS:
+    OUTPUT_DIRECTORY = "/mount/SDC/PROJECTION_RESULTS"
+
 NUMBER_OF_CLASSES  = 13
 DATA_FOLDER = "/mount/SDC/casava/split-data/output"
+WEIGHTS_PATH = "./weights-improvement-39-0.85.hdf5"
+ORIGINAL_IMAGES_PATH = "/mount/SDC/casava/new_data/Dropbox/casava"
+
 img_width, img_height = 224, 224
 crop_width, crop_height = 224, 224
 
@@ -53,41 +59,40 @@ x = Dense(NUMBER_OF_CLASSES, name="dense_3")(x)
 predictions = Activation("softmax", name="activation")(x)
 # this is the model we will train
 model = Model(input=base_model.input, output=predictions)
-WEIGHTS_PATH = "./weights-improvement-39-0.85.hdf5"
 model.load_weights(WEIGHTS_PATH)
 
-#Build force-convolutionalized model for Projection
-base_model = VGG16(weights=None, input_tensor=Input((3, None, None)),include_top=False)
-x = base_model.output
-x = Convolution2D(4096,7,7,activation="relu",name="dense_1")(x)
-x = Convolution2D(4096,1,1,activation="relu",name="dense_2")(x)
-x = Convolution2D(NUMBER_OF_CLASSES,1,1,name="dense_3")(x)
-_output = Softmax4D(axis=1,name="softmax")(x)
-_projection_model = Model(input=base_model.input, output=_output)
+if PLOT_PROJECTIONS:
+    #Build force-convolutionalized model for Projection
+    base_model = VGG16(weights=None, input_tensor=Input((3, None, None)),include_top=False)
+    x = base_model.output
+    x = Convolution2D(4096,7,7,activation="relu",name="dense_1")(x)
+    x = Convolution2D(4096,1,1,activation="relu",name="dense_2")(x)
+    x = Convolution2D(NUMBER_OF_CLASSES,1,1,name="dense_3")(x)
+    _output = Softmax4D(axis=1,name="softmax")(x)
+    _projection_model = Model(input=base_model.input, output=_output)
 
-#Copy weights from the prediction model to the projection model
-for layer in _projection_model.layers:
-    if layer.name.split("_")[-1][:-1] == "conv":
-        orig_layer = model.get_layer(layer.name)
-        layer.set_weights(orig_layer.get_weights())
-        print("Copying layer weights for :", layer.name)
-    elif layer.name.startswith("dense_"):
-        orig_layer = model.get_layer(layer.name)
-        W,b = orig_layer.get_weights()
-        n_filter,previous_filter,ax1,ax2 = layer.get_weights()[0].shape
-        new_W = W.reshape((previous_filter,ax1,ax2,n_filter))
-        new_W = new_W.transpose((3,0,1,2))
-        new_W = new_W[:,:,::-1,::-1]
-        layer.set_weights([new_W,b])
-        print("Adapting and copying layer weights for :", layer.name)
+    #Copy weights from the prediction model to the projection model
+    for layer in _projection_model.layers:
+        if layer.name.split("_")[-1][:-1] == "conv":
+            orig_layer = model.get_layer(layer.name)
+            layer.set_weights(orig_layer.get_weights())
+            print("Copying layer weights for :", layer.name)
+        elif layer.name.startswith("dense_"):
+            orig_layer = model.get_layer(layer.name)
+            W,b = orig_layer.get_weights()
+            n_filter,previous_filter,ax1,ax2 = layer.get_weights()[0].shape
+            new_W = W.reshape((previous_filter,ax1,ax2,n_filter))
+            new_W = new_W.transpose((3,0,1,2))
+            new_W = new_W[:,:,::-1,::-1]
+            layer.set_weights([new_W,b])
+            print("Adapting and copying layer weights for :", layer.name)
 
 
 #Compile both the models
 model.compile(optimizer=SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True), loss='categorical_crossentropy', metrics=["accuracy"])
-_projection_model.compile(optimizer=SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True), loss='categorical_crossentropy', metrics=["accuracy"])
+if PLOT_PROJECTIONS:
+    _projection_model.compile(optimizer=SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True), loss='categorical_crossentropy', metrics=["accuracy"])
 
-
-ORIGINAL_IMAGES_PATH = "/mount/SDC/casava/new_data/Dropbox/casava"
 DATA_FOLDER = "/mount/SDC/casava/split-data/output/test-trimmed"
 
 """
@@ -101,8 +106,7 @@ for _file in glob.glob(ORIGINAL_IMAGES_PATH+"/*/*.JPG"):
     else:
         _high_res_map[_fileName] = _file
 
-classMap = os.listdir(DATA_FOLDER)
-TEST_FILES = glob.glob(DATA_FOLDER+"/*/*.JPG")
+TEST_FILES = glob.glob(validation_data_dir+"/*/*.JPG")
 X = []
 X_PATH = []
 Y = []
@@ -124,16 +128,22 @@ for _image in TEST_FILES:
 
 
 BATCH_SIZE = 32
-test_datagen = keras_image.ImageDataGenerator(rescale=1./255)
-# test_datagen = ImageDataGenerator(rescale=1./255)
-
+if PLOT_PROJECTIONS:
+    test_datagen = keras_image.ImageDataGenerator(rescale=1./255)
+    validation_generator = test_datagen.flow_from_directory(
+            validation_data_dir,
+            target_size=(img_width, img_height),
+            batch_size=BATCH_SIZE,
+            class_mode='categorical',
+            high_res_map=_high_res_map,
+            high_res_target_size=(2000, 2000))
+else:
+    test_datagen = ImageDataGenerator(rescale=1./255)
 validation_generator = test_datagen.flow_from_directory(
         validation_data_dir,
         target_size=(img_width, img_height),
         batch_size=BATCH_SIZE,
-        class_mode='categorical',
-        high_res_map=_high_res_map,
-        high_res_target_size=(2000, 2000))
+        class_mode='categorical')
 
 classMap = [0]*len(validation_generator.class_indices.keys())
 for _key in validation_generator.class_indices.keys():
@@ -148,58 +158,58 @@ for iteration_number, data in enumerate(validation_generator):
     y_pred = np.array([x.argmax() for x in y_pred]).tolist()
     y_true = data[1]
     y_true = np.array([x.argmax() for x in y_true]).tolist()
-    high_res_images = data[2]
-    batch_filenames = data[3]
-
     PREDS += y_pred
     TRUE_LABELS += y_true
 
     print("Y-PRED : ",y_pred)
     print("Y-TRUE : ", y_true)
 
-    for _idx, _high_res_img in enumerate(high_res_images):
-        print("Predicted : ", classMap[y_pred[_idx]], " Actual :", classMap[y_true[_idx]])
-        _projection = _projection_model.predict(_high_res_img.reshape((1,) + _high_res_img.shape))
-        basewidth = 400
-        hsize = 400
-        if classMap[y_pred[_idx]] == classMap[y_true[_idx]]:
-            result = "CORRECT"
-            proj = _projection[0, y_pred[_idx]]
-            resized_proj = np.asarray(Image.fromarray(proj).resize((basewidth, hsize), Image.ANTIALIAS))
-            DIR = OUTPUT_DIRECTORY+"/"+result+"/"+classMap[y_true[_idx]]+"/"+str(uuid.uuid4())+".JPG"
-            try:
-                os.makedirs(DIR)
-            except:
-                pass
-            PATH_TO_PROJECTION = DIR + "/PREDICTED_PROJECTION.png"
-            plt.imsave(PATH_TO_PROJECTION, resized_proj)
-            PATH_TO_ORIGINAL_IMAGE = DIR+"/ORIGINAL_IMAGE.png"
-            keras_image.array_to_img(_high_res_img).resize((basewidth, hsize), Image.ANTIALIAS).save(PATH_TO_ORIGINAL_IMAGE)
-            _result_object = {"predicted":classMap[y_pred[_idx]], "actual": classMap[y_true[_idx]], "filepath": batch_filenames[_idx]}
-            with open(DIR+'/result.json', 'w') as outfile:
-                json.dump(_result_object, outfile)
-        else:
-            result = "INCORRECT"
-            proj = _projection[0, y_pred[_idx]]
-            resized_proj = np.asarray(Image.fromarray(proj).resize((basewidth, hsize), Image.ANTIALIAS))
-            DIR = OUTPUT_DIRECTORY+"/"+result+"/"+classMap[y_true[_idx]]+"/"+str(uuid.uuid4())+".JPG"
-            try:
-                os.makedirs(DIR)
-            except:
-                pass
-            PATH_TO_PROJECTION = DIR+"/PREDICTED_PROJECTION.png"
-            plt.imsave(PATH_TO_PROJECTION, resized_proj)
-            #Render projection for the actual class of the Image
-            proj = _projection[0, y_true[_idx]]
-            resized_proj = np.asarray(Image.fromarray(proj).resize((basewidth, hsize), Image.ANTIALIAS))
-            PATH_TO_PROJECTION = DIR+"/TRUELABELs_PROJECTION.png"
-            plt.imsave(PATH_TO_PROJECTION, resized_proj)
+    if PLOT_PROJECTIONS:
+        high_res_images = data[2]
+        batch_filenames = data[3]
+        for _idx, _high_res_img in enumerate(high_res_images):
+            print("Predicted : ", classMap[y_pred[_idx]], " Actual :", classMap[y_true[_idx]])
+            _projection = _projection_model.predict(_high_res_img.reshape((1,) + _high_res_img.shape))
+            basewidth = 400
+            hsize = 400
+            if classMap[y_pred[_idx]] == classMap[y_true[_idx]]:
+                result = "CORRECT"
+                proj = _projection[0, y_pred[_idx]]
+                resized_proj = np.asarray(Image.fromarray(proj).resize((basewidth, hsize), Image.ANTIALIAS))
+                DIR = OUTPUT_DIRECTORY+"/"+result+"/"+classMap[y_true[_idx]]+"/"+str(uuid.uuid4())+".JPG"
+                try:
+                    os.makedirs(DIR)
+                except:
+                    pass
+                PATH_TO_PROJECTION = DIR + "/PREDICTED_PROJECTION.png"
+                plt.imsave(PATH_TO_PROJECTION, resized_proj)
+                PATH_TO_ORIGINAL_IMAGE = DIR+"/ORIGINAL_IMAGE.png"
+                keras_image.array_to_img(_high_res_img).resize((basewidth, hsize), Image.ANTIALIAS).save(PATH_TO_ORIGINAL_IMAGE)
+                _result_object = {"predicted":classMap[y_pred[_idx]], "actual": classMap[y_true[_idx]], "filepath": batch_filenames[_idx]}
+                with open(DIR+'/result.json', 'w') as outfile:
+                    json.dump(_result_object, outfile)
+            else:
+                result = "INCORRECT"
+                proj = _projection[0, y_pred[_idx]]
+                resized_proj = np.asarray(Image.fromarray(proj).resize((basewidth, hsize), Image.ANTIALIAS))
+                DIR = OUTPUT_DIRECTORY+"/"+result+"/"+classMap[y_true[_idx]]+"/"+str(uuid.uuid4())+".JPG"
+                try:
+                    os.makedirs(DIR)
+                except:
+                    pass
+                PATH_TO_PROJECTION = DIR+"/PREDICTED_PROJECTION.png"
+                plt.imsave(PATH_TO_PROJECTION, resized_proj)
+                #Render projection for the actual class of the Image
+                proj = _projection[0, y_true[_idx]]
+                resized_proj = np.asarray(Image.fromarray(proj).resize((basewidth, hsize), Image.ANTIALIAS))
+                PATH_TO_PROJECTION = DIR+"/TRUELABELs_PROJECTION.png"
+                plt.imsave(PATH_TO_PROJECTION, resized_proj)
 
-            PATH_TO_ORIGINAL_IMAGE = DIR+"/ORIGINAL_IMAGE.png"
-            keras_image.array_to_img(_high_res_img).resize((basewidth, hsize), Image.ANTIALIAS).save(PATH_TO_ORIGINAL_IMAGE)
-            _result_object = {"predicted":classMap[y_pred[_idx]], "actual": classMap[y_true[_idx]], "filepath": batch_filenames[_idx]}
-            with open(DIR+'/result.json', 'w') as outfile:
-                json.dump(_result_object, outfile)
+                PATH_TO_ORIGINAL_IMAGE = DIR+"/ORIGINAL_IMAGE.png"
+                keras_image.array_to_img(_high_res_img).resize((basewidth, hsize), Image.ANTIALIAS).save(PATH_TO_ORIGINAL_IMAGE)
+                _result_object = {"predicted":classMap[y_pred[_idx]], "actual": classMap[y_true[_idx]], "filepath": batch_filenames[_idx]}
+                with open(DIR+'/result.json', 'w') as outfile:
+                    json.dump(_result_object, outfile)
 
     if (iteration_number+1)*BATCH_SIZE >= len(Y):
         break
